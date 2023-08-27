101b237489c2ef189acaaf23c64c76dd83550e82
ari hlfgut <ari@0541234.com>
2023-08-27 10:44:55 +0300

    I finished exercise 1

--- index.ts
+++ index.ts
@@ -1,11 +1,43 @@
 // את הקוד שלכם תכתבו כאן
 
 //  1. Rectangle - מלבן
-
-//  2. Square - ריבוע
-
-// 3. Shape - צורה
+class Rectangle {
+  width: number;
+  height: number;
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+  area(): number {
+    return this.width * this.height;
+  }
+}
 
 // 4. Method Chaining
 
 // 5. Shape with draw method
+
+// // את הקוד שלכם תכתבו כאן
+
+// //  1. Rectangle - מלבן
+// class Rectangle {
+//     width: number;
+//     height: number;
+//     constructor(width: number, height: number) {
+//       this.width = width;
+//       this.height = height;
+//     }
+//     area(): number {
+//       this.km += 10;
+//       return;
+//     }
+//     getCarData(): string {
+//       return `A ${this.model} ${this.color} car with ${this.km} kilometers`;
+//     }
+//   }
+//   let car1 = new Car("red", "2020", 1000);
+//   car1.drive();
+//   car1.getCarData();
+//   // A 2020 red car with 1010 kilometers
+
+//   console.log(car1);
